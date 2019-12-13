b2fc916b965fa7af55628ecb1a5fef086e90c451
Sokolovskyy Roman <roman.sokolovskyy@konicaminolta.cz>
2019-12-13 14:31:49 +0100

    qWER

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -16,7 +16,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('artgris_file_manager');
-        $rootNode = $treeBuilder->root();
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
